f8822ec4508e969c08ae012fb69df561d1daabe8
戈剑 廖 <lgjsword@outlook.com>
2018-09-03 18:54:55 +0800

    添加项目文件。

---
+++ Project/main.cpp
@@ -0,0 +1,78 @@
+#include <opencv2\opencv.hpp>
+#include <opencv2\highgui\highgui.hpp>
+#include <opencv2\imgproc\imgproc.hpp>
+using namespace cv;
+
+
+void imageDisplay(void)
+{
+	Mat srcImage = imread("test.png");
+	imshow("原始图片", srcImage);
+	waitKey();
+}
+
+void imageCorr(void)
+{
+	Mat srcImage = imread("test.png");
+	imshow("原图", srcImage);
+	//腐蚀
+	Mat element = getStructuringElement(MORPH_RECT, Size(15, 15));
+	Mat dstImage;
+	erode(srcImage, dstImage, element);
+
+	imshow("腐蚀效果", dstImage);
+
+	waitKey();
+}
+
+void imageFilter(void)
+{
+	Mat srcImage = imread("test.png");
+	imshow("原图", srcImage);
+	Mat dstImage;
+	blur(srcImage, dstImage, Size(7, 7));
+
+	imshow("均值滤波效果", dstImage);
+	waitKey();
+
+}
+
+void imageCanny(void)
+{
+	Mat srcImage = imread("test.png");
+	imshow("原图", srcImage);
+	
+	Mat edge, grayImage;
+
+	cvtColor(srcImage, grayImage, CV_BGR2GRAY);
+
+	blur(grayImage, edge, Size(3, 3));
+
+	Canny(edge, edge, 3, 9, 3);
+
+	imshow(" 边缘检测", edge);
+
+	waitKey();
+}
+
+void videoAcq(void)
+{
+	VideoCapture capture(0);
+	while (1) {
+		Mat frame;
+		capture >> frame;
+		imshow("video", frame);
+		waitKey(10);
+	}
+
+}
+
+int main()
+{
+	imageDisplay();
+	imageCorr();
+	imageFilter();
+	imageCanny();
+	videoAcq();
+	return 0;
+}

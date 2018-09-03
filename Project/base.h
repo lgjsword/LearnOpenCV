08aaa42906ba8d7710d088ea7ed22021acca019f
戈剑 廖 <lgjsword@outlook.com>
2018-09-03 21:01:35 +0800

    21:01

---
+++ Project/base.h
@@ -0,0 +1,11 @@
+#pragma once
+#include <opencv2\opencv.hpp>
+#include <opencv2\highgui\highgui.hpp>
+#include <opencv2\imgproc\imgproc.hpp>
+
+
+void imageDisplay(void);
+void imageCorr(void);
+void imageFilter(void);
+void imageCanny(void);
+void videoAcq(void);
